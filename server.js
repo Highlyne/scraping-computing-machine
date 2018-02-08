42a5b200b07d03b81192d07920a8359f152d2e19
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-02-08 07:21:31 -0500

    adding mLab database connection

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Set up the Express App
+
+// Set up the Express App & Mongo Database
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 // Set up handlebars
 // ================================================================
 var exphbs = require("express-handlebars");
@@ -22,9 +23,11 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB and tell mongoose to use promises as callbacks
 // ==============================================================
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/nprscraperdb");
+// mongoose.Promise = Promise;
+// mongoose.connect("mongodb://localhost/nprscraperdb");
 
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {useMongoClient: true});
 // Use morgan logger for logging requests
 // =============================================
 app.use(logger("dev"));
